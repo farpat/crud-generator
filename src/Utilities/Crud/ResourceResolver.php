9bfaae857e376b04c757086e05a5d2ec4e08cfee
Patrick <pfarrugia@digitick.com>
2018-10-11 14:55:02 +0200

    feature (ResourceResolver) Add the 'findAll' function that allow making joins

--- src/Utilities/Crud/ResourceResolver.php
+++ src/Utilities/Crud/ResourceResolver.php
@@ -3,12 +3,10 @@
 namespace App\Utilities\Crud;
 
 use Doctrine\Common\Annotations\Reader;
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\Common\Persistence\{ObjectManager, ObjectRepository};
+use Doctrine\ORM\Mapping\{Annotation, ManyToMany, ManyToOne, OneToMany, OneToOne};
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\{Extension\Core\Type\FormType, FormBuilderInterface, FormFactoryInterface};
 
 /**
  * Cette classe prend " en entrée " un nom de ressource (exemples : post, category, comment, etc.)
@@ -87,7 +85,7 @@ class ResourceResolver
                 $properties[$property->name] = $annotation;
             } elseif ($type === 'create' && $annotation && $annotation->showInCreate !== false) { //si pas d'annotation par défaut on n'affiche pas
                 $properties[$property->name] = $annotation;
-            } elseif ($type === 'edit'  && $annotation && $annotation->showInEdit !== false) { //si pas d'annotation par défaut on n'affiche pas
+            } elseif ($type === 'edit' && $annotation && $annotation->showInEdit !== false) { //si pas d'annotation par défaut on n'affiche pas
                 $properties[$property->name] = $annotation;
             }
         }
@@ -135,7 +133,7 @@ class ResourceResolver
     }
 
     /**
-     *
+     * Renvoie un " FormBuilder " de création ou d'édition selon $data
      * @param mixed $data Entité à hydrater par le formulaire
      *
      * @return FormBuilderInterface
@@ -174,6 +172,70 @@ class ResourceResolver
     {
         $this->verifyExistenceOfClasses();
         return $this->manager->getRepository($this->resolveEntityClassName());
+    }
+
+    /**
+     * Renvoie un tableau contenant les lignes de la ressource courante
+     * @return array
+     * @throws \Exception
+     */
+    public function findAll (): array
+    {
+        $propertiesWithRelation = $this->getPropertiesWithRelation();
+        $repository = $this->resolveRepository();
+
+        if (empty($propertiesWithRelation)) {
+            $repository->findAll();
+        }
+
+        $builder = $repository->createQueryBuilder('table');
+        $selects = ['table'];
+        foreach ($propertiesWithRelation as $property => $annotation) {
+            $builder->join('table.' . $property, $property);
+            $selects[] = $property;
+        }
+
+        return $builder->select($selects)->getQuery()->getResult();
+    }
+
+    /**
+     * Renvoie les propriétés contenant une annotation de relation
+     * @return array
+     * @throws \ReflectionException
+     */
+    private function getPropertiesWithRelation (): array
+    {
+        $rf = new \ReflectionClass($this->resolveEntityClassName());
+
+        $properties = [];
+        foreach ($rf->getProperties() as $property) {
+            if ($annotation = $this->getRelationAnnotation($property)) {
+                $properties[$property->name] = $annotation;
+            }
+        }
+
+        return $properties;
+    }
+
+    /**
+     * Renvoie l'annotation de relation (ou null)
+     *
+     * @param \ReflectionProperty $property
+     *
+     * @return Annotation|null
+     */
+    private function getRelationAnnotation (\ReflectionProperty $property): ?Annotation
+    {
+        $annotations = $this->reader->getPropertyAnnotations($property);
+
+        foreach ($annotations as $annotation) {
+            $annotationClass = get_class($annotation);
+            if (in_array($annotationClass, [OneToOne::class, ManyToMany::class, OneToMany::class, ManyToOne::class])) {
+                return $annotation;
+            }
+        }
+
+        return null;
     }
 
     /**
